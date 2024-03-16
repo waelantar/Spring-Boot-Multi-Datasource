5bd2138f39939c6950e9542dd371e78337bbc544
Wael Antar <antarwael189@gmail.com>
2024-03-16 16:54:41 +0100

    Initial commit:Project setup

---
+++ src/test/java/com/example/springbootmultidatasource/SpringBootMultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
